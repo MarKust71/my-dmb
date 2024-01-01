a46f8e928a07dae97eb19796724986985db8b7ed
Marek Kustosz <markust71@hotmail.com>
2024-01-01 14:55:14 +0100

    Basic setup

---
+++ src/app/body-cleansing-program/page.tsx
@@ -0,0 +1,29 @@
+/*
+import {
+  PictogramPhone,
+  PictogramProgram,
+  PictogramSociety,
+} from '@/components/svgs'
+*/
+import MaxWidthWrapper from '@/components/max-width-wrapper'
+
+const BodyCleansingProgram = () => {
+  return (
+    <div
+      className={`min-h-screen bg-[url('/img/body-cleansing-program/background-vertical.png')] md:bg-[url('/img/body-cleansing-program/body-cleansing-bundle-background.png')] bg-no-repeat bg-cover bg-left-top`}
+    >
+      <MaxWidthWrapper>
+        <></>
+        {/*
+        <PictogramProgram />
+
+        <PictogramSociety />
+
+        <PictogramPhone />
+*/}
+      </MaxWidthWrapper>
+    </div>
+  )
+}
+
+export default BodyCleansingProgram
