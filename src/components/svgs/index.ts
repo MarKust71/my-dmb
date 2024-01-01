75066422dc50c832736608e27426fe40c16636c9
Marek Kustosz <markust71@hotmail.com>
2024-01-01 14:57:12 +0100

    Basic setup (#1)

---
+++ src/components/svgs/index.ts
@@ -0,0 +1,3 @@
+export { default as PictogramProgram } from './pictogram-program-min.svg'
+export { default as PictogramSociety } from './pictogram-society-min.svg'
+export { default as PictogramPhone } from './pictogram-phone-min.svg'
