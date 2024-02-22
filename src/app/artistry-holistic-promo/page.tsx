1af314354b6d04894f6c00c7c964eb9981af643c
Marek Kustosz <markust71@hotmail.com>
2024-02-22 23:38:58 +0100

    refactor carousel (#10)

--- src/app/artistry-holistic-promo/page.tsx
+++ src/app/artistry-holistic-promo/page.tsx
@@ -2,7 +2,6 @@ import MaxWidthWrapper from '@/components/max-width-wrapper'
 import { ArtistryLogotype } from '@/components/svgs'
 
 import './page.scss'
-import Image from 'next/legacy/image'
 import {
   Carousel,
   CarouselContent,
@@ -28,8 +27,9 @@ const ArtistryHolisticPromoPage = () => {
 
         <div className={'mb-4'} />
 
-        <article className={'[text-shadow:_2px_2px_2px_rgb(0_0_0_/_0.4)]'}>
+        <article className={'text-shadow'}>
           <div className={'paragraph-shape'} />
+
           <h1 className={'text-2xl font-bold mb-2'}>
             Holistyczne zestawy Artistry
           </h1>
@@ -45,40 +45,57 @@ const ArtistryHolisticPromoPage = () => {
 
         <div className={'mb-4'} />
 
+        <h1 className={'text-2xl font-bold mb-2 text-shadow'}>
+          Dobierz zestaw do potrzeb swojej Pani
+        </h1>
+
         <Carousel>
           <CarouselContent>
             <CarouselItem>
               <CarouselItemContent
+                href={'/artistry-holistic-promo?item=320868'}
+                title={'Holistyczny nawilżający zestaw Artistry™'}
+                subtitle={
+                  'Do młodej skóry, która wymaga jedynie kompleksowej pielęgnacji'
+                }
                 src={'/img/artistry/320868_PL_IMAGE_product-image_800_800.jpg'}
                 alt={'Artistry Holistic Promo'}
-                width={400}
-                height={400}
               />
             </CarouselItem>
+
             <CarouselItem>
               <CarouselItemContent
-                src={'/img/artistry/320874_PL_IMAGE_product-image_800_800.jpg'}
-                alt={'Artistry Holistic Promo'}
-                width={400}
-                height={400}
-              />
-            </CarouselItem>
-            <CarouselItem>
-              <CarouselItemContent
+                href={'/artistry-holistic-promo?item=320871'}
+                title={'Holistyczny regenerujący zestaw Artistry™'}
+                subtitle={
+                  'Do skóry, na której widoczne są pierwsze oznaki zmęczenia i którą trzeba odżywić oraz ujędrnić'
+                }
                 src={'/img/artistry/320871_PL_IMAGE_product-image_800_800.jpg'}
                 alt={'Artistry Holistic Promo'}
-                width={400}
-                height={400}
+              />
+            </CarouselItem>
+
+            <CarouselItem>
+              <CarouselItemContent
+                href={'/artistry-holistic-promo?item=320874'}
+                title={'Holistyczny ujędrniający zestaw Artistry™'}
+                subtitle={
+                  'Do skóry wykazującej zaawansowane oznaki zmęczenia, potrzebującej rozjaśnienia i ujędrnienia'
+                }
+                src={'/img/artistry/320874_PL_IMAGE_product-image_800_800.jpg'}
+                alt={'Artistry Holistic Promo'}
               />
             </CarouselItem>
           </CarouselContent>
+
           <CarouselPrevious />
-          <CarouselNext />{' '}
+
+          <CarouselNext />
         </Carousel>
 
         <div className={'mb-3'} />
 
-        <p className={'text-gray-600 text-xs/3'}>
+        <p className={'text-gray-600 text-xs/3 pb-5'}>
           * Produkt Nutrilite™ Biotin C Plus zawiera biotynę i witaminę C.
           Biotyna pomaga zachować zdrową skórę. Witamina C pomaga w prawidłowej
           produkcji kolagenu w celu zapewnienia prawidłowego funkcjonowania
