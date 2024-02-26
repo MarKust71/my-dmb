f7bfb3536988785e0e5d05f070632758c012cd41
Marek Kustosz <markust71@hotmail.com>
2024-02-26 23:17:26 +0100

    add info modal

--- src/components/artistry/carousel-item-content/carousel-item-content.tsx
+++ src/components/artistry/carousel-item-content/carousel-item-content.tsx
@@ -8,6 +8,15 @@ import { PrevButton } from './prev-button'
 import { NextButton } from './next-button'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer'
 
 export const CarouselItemContent = ({
   href = '',
@@ -49,19 +58,57 @@ export const CarouselItemContent = ({
     <>
       <CarouselItemContentHeader />
 
-      <CarouselItemContentImage
-        src={src}
-        alt={alt}
-        width={400}
-        height={400}
-        href={href}
-      />
+      <Drawer>
+        <DrawerTrigger className={'w-full'}>
+          <CarouselItemContentImage
+            src={src}
+            alt={alt}
+            width={400}
+            height={400}
+            href={href}
+          />
 
-      <Link href={href} target={'_blank'}>
-        <Button variant={'destructive'} className={'w-full mt-4 text-2xl'}>
-          Ten klikam i zamawiam!
-        </Button>
-      </Link>
+          <Button variant={'destructive'} className={'w-full mt-4 text-2xl'}>
+            Ten klikam i zamawiam!
+          </Button>
+        </DrawerTrigger>
+
+        <DrawerContent>
+          <div className="mx-auto w-full max-w-sm">
+            <DrawerHeader>
+              <DrawerTitle>Doskonały wybór!</DrawerTitle>
+            </DrawerHeader>
+
+            <p className={'p-4 text-justify'}>
+              Za chwilę nastąpi przekierowanie do naszego sklepu internetowego.
+            </p>
+
+            <p className={'px-4 text-justify'}>
+              Finalizacja zakupu wymaga posiadania indywidualnego konta
+              w&nbsp;serwisie. Założenie konta jest bezpłatne i&nbsp;zajmuje
+              tylko chwilę. Posiadanie konta nie pociąga za sobą żadnych
+              zobowiązań ani kosztów w&nbsp;przyszłości, a&nbsp;będzie jedynie
+              ułatwieniem w&nbsp;przypadku potrzeby złożenia reklamacji,
+              dokonania wymiany albo zwrotu zakupionego towaru.
+            </p>
+            <DrawerFooter>
+              <DrawerClose asChild>
+                <Link href={href} target={'_blank'}>
+                  <Button variant={'destructive'} className={'w-full'}>
+                    Idę dalej
+                  </Button>
+                </Link>
+              </DrawerClose>
+
+              <DrawerClose asChild>
+                <Button variant="ghost" className={'text-gray-500'}>
+                  Wracam
+                </Button>
+              </DrawerClose>
+            </DrawerFooter>
+          </div>
+        </DrawerContent>
+      </Drawer>
     </>
   )
 }
