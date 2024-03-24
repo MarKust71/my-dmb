82cb59dc28b1d65d512995ddd137b2e9df6ffeed
Marek Kustosz <markust71@hotmail.com>
2024-03-24 12:08:33 +0100

    dashboard quick adjustment (#31)

--- src/components/dashboard/dashboard-wrapper.tsx
+++ src/components/dashboard/dashboard-wrapper.tsx
@@ -6,7 +6,7 @@ export const DashboardWrapper = ({ children }: DashboardWrapperProps) => {
   return (
     <div
       className={
-        'h-full flex flex-col items-start justify-start bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800'
+        'w-full h-full flex flex-col items-start justify-start bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800'
       }
     >
       {children}
