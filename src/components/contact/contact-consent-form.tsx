a1145c9a9a0d9f16f8115de1c8c0bc6271fe5c77
Marek Kustosz <markust71@hotmail.com>
2024-03-28 16:30:56 +0100

    no rodo (#38)

---
+++ src/components/contact/contact-consent-form.tsx
@@ -0,0 +1,41 @@
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from '@/components/ui/form'
+import { Checkbox } from '@/components/ui/checkbox'
+import Link from 'next/link'
+import { ContactConsentFormProps } from '@/components/contact/contact-consent-form.types'
+
+export const ContactConsentForm = ({ control }: ContactConsentFormProps) => {
+  return (
+    <FormField
+      control={control}
+      name="gdprConsent"
+      render={({ field }) => (
+        <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4 shadow">
+          <FormControl>
+            <Checkbox
+              className={'border-gray-200'}
+              checked={field.value}
+              onCheckedChange={field.onChange}
+            />
+          </FormControl>
+          <div className="space-y-1 leading-none">
+            <FormLabel>Zgoda RODO</FormLabel>
+
+            <FormDescription>
+              Pełna treść udzielanej zgody{' '}
+              <Link href="/examples/forms" className={'font-bold'}>
+                TUTAJ
+              </Link>
+              .
+            </FormDescription>
+          </div>
+        </FormItem>
+      )}
+    />
+  )
+}
---
+++ src/components/contact/contact-consent-form.types.ts
@@ -0,0 +1,6 @@
+import { Control } from 'react-hook-form'
+import { ContactFormValues } from '@/components/contact/contact-form.types'
+
+export type ContactConsentFormProps = {
+  control: Control<ContactFormValues, any>
+}
