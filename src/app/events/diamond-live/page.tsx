5a30b6af00f493df045b48b3a43456c3a6f9ef5b
Marek Kustosz <markust71@hotmail.com>
2024-04-21 22:49:06 +0200

    add diamond-event page - mobile (#56)

---
+++ src/app/events/diamond-live/page.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+
+import DiamondLiveImage from '@/assets/images/diamond-live-image.jpeg'
+import { Button } from '@/components/ui/button'
+
+const DiamondLivePage = () => {
+  return (
+    <div className={'min-h-screen bg-black'}>
+      <Image src={DiamondLiveImage} alt={'diamond-live-image'} />
+
+      <a
+        href={'https://us06web.zoom.us/j/89796337110'}
+        className={'flex justify-center py-4 mx-auto'}
+      >
+        <Button className={'bg-[#074CE5]'}>Dołącz do spotkania ZOOM</Button>
+      </a>
+    </div>
+  )
+}
+
+export default DiamondLivePage
