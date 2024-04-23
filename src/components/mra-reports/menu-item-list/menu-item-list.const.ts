660e736fc6bffb2b06f1c4243175e9f2bfaa8966
Marek Kustosz <markust71@hotmail.com>
2024-04-23 22:20:25 +0200

    MRA reports - encode uri fix (#60)

--- src/components/mra-reports/menu-item-list/menu-item-list.const.ts
+++ src/components/mra-reports/menu-item-list/menu-item-list.const.ts
@@ -2,67 +2,72 @@ export const reportItems = [
   {
     id: 'item-01',
     title: '01-Układ krążenia i naczyń mózgowych',
-    url: '/mra-reports/[id]/01-Układ krążenia i naczyń mózgowych.htm',
+    url: '/mra-reports/[id]/01-Układ krążenia i naczyń mózgowych.htm',
+  },
+  {
+    id: 'item-01',
+    title: '01-Układ krążenia i naczyń mózgowych',
+    url: '/mra-reports/[id]/01-Uklad_krazenia_i_naczyn_mozgowych.htm',
   },
   {
     id: 'item-02',
     title: '02-Funkcje przewodu pokarmowego',
-    url: '/mra-reports/[id]/02-Funkcje przewodu pokarmowego.htm',
+    url: '/mra-reports/[id]/02-Funkcje_przewodu_pokarmowego.htm',
   },
   {
     id: 'item-03',
     title: '03-Funkcje wątroby',
-    url: '/mra-reports/[id]/03-Funkcje wątroby.htm',
+    url: '/mra-reports/[id]/03-Funkcje_watroby.htm',
   },
   {
     id: 'item-04',
     title: '04-Funkcje pęcherzyka żółciowego',
-    url: '/mra-reports/[id]/04-Funkcje pęcherzyka żółciowego.htm',
+    url: '/mra-reports/[id]/04-Funkcje_pecherzyka_zolciowego.htm',
   },
   {
     id: 'item-05',
     title: '05-Funkcje trzustki',
-    url: '/mra-reports/[id]/05-Funkcje trzustki.htm',
+    url: '/mra-reports/[id]/05-Funkcje_trzustki.htm',
   },
   {
     id: 'item-06',
     title: '06-Funkcje nerek',
-    url: '/mra-reports/[id]/06-Funkcje nerek.htm',
+    url: '/mra-reports/[id]/06-Funkcje_nerek.htm',
   },
   {
     id: 'item-07',
     title: '07-Funkcje płuc',
-    url: '/mra-reports/[id]/07-Funkcje płuc.htm',
+    url: '/mra-reports/[id]/07-Funkcje_pluc.htm',
   },
   {
     id: 'item-08',
     title: '08-Mózg',
-    url: '/mra-reports/[id]/08-Mózg.htm',
+    url: '/mra-reports/[id]/08-Mozg.htm',
   },
   {
     id: 'item-09',
     title: '09-Układ kostny',
-    url: '/mra-reports/[id]/09-Układ kostny.htm',
+    url: '/mra-reports/[id]/09-Uklad_kostny.htm',
   },
   {
     id: 'item-10',
     title: '10-Gęstość mineralna kości',
-    url: '/mra-reports/[id]/10-Gęstość mineralna kości.htm',
+    url: '/mra-reports/[id]/10-Gestosc_mineralna_kosci.htm',
   },
   {
     id: 'item-11',
     title: '11-Choroby reumatyczne kości',
-    url: '/mra-reports/[id]/11-Choroby reumatyczne kości.htm',
+    url: '/mra-reports/[id]/11-Choroby_reumatyczne_kosci.htm',
   },
   {
     id: 'item-12',
     title: '12-Cukier we krwi',
-    url: '/mra-reports/[id]/12-Cukier we krwi.htm',
+    url: '/mra-reports/[id]/12-Cukier_we_krwi.htm',
   },
   {
     id: 'item-13',
     title: '13-Ogólny stan fizyczny',
-    url: '/mra-reports/[id]/13-Ogólny stan fizyczny.htm',
+    url: '/mra-reports/[id]/13-Ogolny_stan_fizyczny.htm',
   },
   {
     id: 'item-14',
@@ -72,7 +77,7 @@ export const reportItems = [
   {
     id: 'item-15',
     title: '15-Pierwiastki śladowe',
-    url: '/mra-reports/[id]/15-Pierwiastki śladowe.htm',
+    url: '/mra-reports/[id]/15-Pierwiastki_sladowe.htm',
   },
   {
     id: 'item-18',
@@ -82,17 +87,17 @@ export const reportItems = [
   {
     id: 'item-19',
     title: '19-Skóra',
-    url: '/mra-reports/[id]/19-Skóra.htm',
+    url: '/mra-reports/[id]/19-Skora.htm',
   },
   {
     id: 'item-20',
     title: '20-Układ hormonalny',
-    url: '/mra-reports/[id]/20-Układ hormonalny.htm',
+    url: '/mra-reports/[id]/20-Uklad_hormonalny.htm',
   },
   {
     id: 'item-21',
     title: '21-Układ odpornościowy',
-    url: '/mra-reports/[id]/21-Układ odpornościowy.htm',
+    url: '/mra-reports/[id]/21-Uklad_odpornosciowy.htm',
   },
   {
     id: 'item-22',
@@ -112,17 +117,17 @@ export const reportItems = [
   {
     id: 'item-25',
     title: '25-Indeks wzrostu kostnego',
-    url: '/mra-reports/[id]/25-Indeks wzrostu kostnego.htm',
+    url: '/mra-reports/[id]/25-Indeks_wzrostu_kostnego.htm',
   },
   {
     id: 'item-26',
     title: '26-Funkcje oczu',
-    url: '/mra-reports/[id]/26-Funkcje oczu.htm',
+    url: '/mra-reports/[id]/26-Funkcje_oczu.htm',
   },
   {
     id: 'item-27',
     title: '27-Metale ciężkie',
-    url: '/mra-reports/[id]/27-Metale ciężkie.htm',
+    url: '/mra-reports/[id]/27-Metale_ciezkie.htm',
   },
   {
     id: 'item-28',
@@ -137,7 +142,7 @@ export const reportItems = [
   {
     id: 'item-30',
     title: '30-Otyłość',
-    url: '/mra-reports/[id]/30-Otyłość.htm',
+    url: '/mra-reports/[id]/30-Otylosc.htm',
   },
   {
     id: 'item-31',
@@ -147,7 +152,7 @@ export const reportItems = [
   {
     id: 'item-32',
     title: '32-Funkcja jelit',
-    url: '/mra-reports/[id]/32-Funkcja jelit.htm',
+    url: '/mra-reports/[id]/32-Funkcja_jelit.htm',
   },
   {
     id: 'item-33',
@@ -157,46 +162,46 @@ export const reportItems = [
   {
     id: 'item-34',
     title: '34-Kwas tłuszczowy',
-    url: '/mra-reports/[id]/34-Kwas tłuszczowy.htm',
+    url: '/mra-reports/[id]/34-Kwas_tluszczowy.htm',
   },
   {
     id: 'item-50',
     title: '50-Meridiany i rozgałęzienia',
-    url: '/mra-reports/[id]/50-Meridiany i rozgałęzienia.htm',
+    url: '/mra-reports/[id]/50-Meridiany_i_rozgalezienia.htm',
   },
   {
     id: 'item-51',
     title: '51-Puls serca i naczyń mózgowych',
-    url: '/mra-reports/[id]/51-Puls serca i naczyń mózgowych.htm',
+    url: '/mra-reports/[id]/51-Puls_serca_i_naczyn_mozgowych.htm',
   },
   {
     id: 'item-52',
     title: '52-Lipidy w krwi',
-    url: '/mra-reports/[id]/52-Lipidy w krwi.htm',
+    url: '/mra-reports/[id]/52-Lipidy_w_krwi.htm',
   },
   {
     id: 'item-54',
     title: '54-Cykl menstruacyjny',
-    url: '/mra-reports/[id]/54-Cykl menstruacyjny.htm',
+    url: '/mra-reports/[id]/54-Cykl_menstruacyjny.htm',
   },
   {
     id: 'item-80',
     title: '80-Fachowe analizy',
-    url: '/mra-reports/[id]/80-Fachowe analizy.htm',
+    url: '/mra-reports/[id]/80-Fachowe_analizy.htm',
   },
   {
     id: 'item-81',
     title: '81-Podręcznik analizy',
-    url: '/mra-reports/[id]/81-Podręcznik analizy.htm',
+    url: '/mra-reports/[id]/81-Podrecznik_analizy.htm',
   },
   {
     id: 'item-99',
     title: '99-Skład ciała',
-    url: '/mra-reports/[id]/99-Skład ciała.htm',
+    url: '/mra-reports/[id]/99-Sklad_ciala.htm',
   },
   {
     id: 'item-00',
     title: '00-Ocena kompleksowa',
-    url: '/mra-reports/[id]/00-Ocena kompleksowa.htm',
+    url: '/mra-reports/[id]/00-Ocena_kompleksowa.htm',
   },
 ]
