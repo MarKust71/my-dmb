9f97bce44694ab8e5fa9fcdbf55ecadf7016a574
Marek Kustosz <markust71@hotmail.com>
2024-05-11 21:46:30 +0200

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,23 @@ export default async function RootLayout({
     <SessionProvider session={session}>
       <html lang="en" className="light">
         <head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
           <link rel="manifest" href="/manifest.json" />
         </head>
         <body
