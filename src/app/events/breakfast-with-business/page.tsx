5e8bc1d7db5381641aa5788bf453e78b89206834
Marek Kustosz <markust71@hotmail.com>
2024-05-12 02:39:30 +0200

    add breakfast with business page

---
+++ src/app/events/breakfast-with-business/page.tsx
@@ -0,0 +1,49 @@
+import Image from 'next/image'
+
+import BreakfastWithBusinessImage from '@/assets/images/breakfast-with-business-image.jpeg'
+import { Button } from '@/components/ui/button'
+
+import './page.scss'
+
+const BreakfastWithBusinessPage = () => {
+  return (
+    <div
+      className={
+        'min-h-screen w-full md:max-w-screen-md md:mx-auto bg-[#FFF2EB]'
+      }
+    >
+      <div className={'absolute w-full md:max-w-screen-md h-40 bg-[#36231D]'} />
+
+      <div className={'p-8 relative z-10'}>
+        <div
+          className={'relative w-full z-10 p-8 pt-0 border-[#422418] border-2'}
+        >
+          <Image
+            className={''}
+            src={BreakfastWithBusinessImage}
+            alt={'breakfast-with-business-image'}
+          />
+
+          <h1 className={'text-2xl font-bold text-[#422418] mt-4 text-center'}>
+            {'Śniadanie z Biznesem'}
+          </h1>
+
+          <h2 className={'text-lg text-[#422418] text-center mt-2'}>
+            {'sobota, godzina 9:00'}
+          </h2>
+
+          <a
+            href={'https://zoom.us/j/981646663'}
+            className={'flex justify-center mx-auto mt-8'}
+          >
+            <Button className={'zoom-button rounded-full'}>
+              Kliknij i dołącz do spotkania ZOOM
+            </Button>
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BreakfastWithBusinessPage
