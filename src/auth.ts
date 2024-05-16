85ff88cc69c2286348f857fb5d881d64391e889f
Marek Kustosz <markust71@hotmail.com>
2024-05-16 18:32:58 +0200

    add user role

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,10 @@
-import NextAuth from 'next-auth'
+import NextAuth, { Session } from 'next-auth'
 import authConfig from '@/auth.config'
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import { db } from '@/lib/db'
+import { JWT } from 'next-auth/jwt'
+import { getUserById } from '@/data/auth/user'
+import { UserRole } from '@prisma/client'
 
 export const {
   handlers: { GET, POST },
@@ -9,6 +12,36 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session(params) {
+      const { session, token } = params as { session: Session; token: JWT }
+
+      if (token.sub && session.user) {
+        session.user.id = token.sub
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole //TODO: Fix this - no 'as'
+      }
+
+      return session
+    },
+    async jwt({ token }) {
+      if (!token.sub) {
+        return token
+      }
+
+      const existingUser = await getUserById(token.sub)
+
+      if (!existingUser) {
+        return token
+      }
+
+      token.role = existingUser.role
+
+      return token
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   ...authConfig,
--- src/components/dashboard/dashboard-session-box.tsx
+++ src/components/dashboard/dashboard-session-box.tsx
@@ -1,22 +1,32 @@
 'use client'
 
 import { useSession } from 'next-auth/react'
+import { DashboardSessionBoxProps } from '@/components/dashboard/dashboard-session-box.types'
 
-export const DashboardSessionBox = () => {
+export const DashboardSessionBox = ({ session }: DashboardSessionBoxProps) => {
   const { data, status, update } = useSession()
 
   return (
     <>
       <h1>Session data:</h1>
+
       <div className={'my-4 px-4 text-balance'}>
         <p>{`${'id'}: ${data?.user?.id}`}</p>
         <p>{`${'email'}: ${data?.user?.email}`}</p>
         <p>{`${'name'}: ${data?.user?.name}`}</p>
+        <p>{`${'role'}: ${data?.user?.role}`}</p>
         <p>{`${'image'}: ${data?.user?.image}`}</p>
         <p>{`${'status'}: ${status}`}</p>
         <p>{`${'expires'}: ${data?.expires}`}</p>
+      </div>
 
-        <p className={'mt-4'}>{JSON.stringify(data)}</p>
+      <div className={'my-4 px-4 text-balance'}>
+        <p>{`${'id'}: ${session?.user?.id}`}</p>
+        <p>{`${'email'}: ${session?.user?.email}`}</p>
+        <p>{`${'name'}: ${session?.user?.name}`}</p>
+        <p>{`${'role'}: ${session?.user?.role}`}</p>
+        <p>{`${'image'}: ${session?.user?.image}`}</p>
+        <p>{`${'expires'}: ${session?.expires}`}</p>
       </div>
     </>
   )
