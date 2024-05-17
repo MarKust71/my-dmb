d61ac6ce320379eeb6af4c0f32920a7f4321c123
Marek Kustosz <markust71@hotmail.com>
2024-05-17 12:05:00 +0200

    set up github signin

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -4,9 +4,14 @@ import { LoginSchema } from '@/schemas'
 import { getUserByEmail } from '@/data/auth/user'
 import bcrypt from 'bcryptjs'
 import Google from 'next-auth/providers/google'
+import Github from 'next-auth/providers/github'
 
 export default {
   providers: [
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
--- src/components/auth/auth-social-buttons.tsx
+++ src/components/auth/auth-social-buttons.tsx
@@ -3,12 +3,12 @@
 import { Button } from '@/components/ui/button'
 import { FcGoogle } from 'react-icons/fc'
 import { FaFacebook, FaInstagram } from 'react-icons/fa'
-import { FaXTwitter } from 'react-icons/fa6'
+import { FaXTwitter, FaGithub } from 'react-icons/fa6'
 import { signIn } from 'next-auth/react'
 import { DEFAULT_LOGIN_REDIRECT } from '@/routes'
 
 export const AuthSocialButtons = () => {
-  const handleClick = (provider: 'google') => {
+  const handleClick = (provider: 'google' | 'github') => {
     signIn(provider, {
       callbackUrl: DEFAULT_LOGIN_REDIRECT,
     })
@@ -23,6 +23,15 @@ export const AuthSocialButtons = () => {
         onClick={() => handleClick('google')}
       >
         <FcGoogle className={'h-5 w-5'} />
+      </Button>
+
+      <Button
+        size={'lg'}
+        className={'w-full px-1'}
+        variant={'outline'}
+        onClick={() => handleClick('github')}
+      >
+        <FaGithub className={'h-5 w-5'} />
       </Button>
 
       <Button
