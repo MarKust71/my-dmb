787277c03424f3403186ed8be040005b85412d11
Marek Kustosz <markust71@hotmail.com>
2024-06-22 18:07:59 +0200

    update next-auth package

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -16,10 +16,44 @@ export default {
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      /*
+      authorization: {
+        params: {
+          access_type: 'offline',
+          prompt: 'consent',
+          response_type: 'code',
+        },
+      },
+*/
     }),
     Facebook({
       clientId: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      /*
+      idToken: true,
+      authorization: {
+        url: 'https://www.facebook.com/v11.0/dialog/oauth',
+        params: {
+          client_id: process.env.FACEBOOK_CLIENT_ID,
+          scope: 'openid email',
+          response_type: 'code',
+        },
+      },
+      token: {
+        url: 'https://graph.facebook.com/oauth/access_token',
+        async request(context) {
+          const url =
+            `https://graph.facebook.com/oauth/access_token` +
+            `?code=${context.params.code}` +
+            `&client_id=${context.provider.clientId}` +
+            `&redirect_uri=${context.provider.callbackUrl}` +
+            `&client_secret=${context.provider.clientSecret}`
+          const response = await fetch(url)
+          const tokens = await response.json()
+          return { tokens }
+        },
+      },
+*/
     }),
     Credentials({
       async authorize(credentials) {
