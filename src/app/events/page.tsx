49ce22509940abaae1f4349369bd5a71a8bbab6a
Marek Kustosz <markust71@hotmail.com>
2024-06-24 20:20:53 +0200

    adjust events main page (#96)

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -7,8 +7,15 @@ import { EventBoxLink } from '@/components/ui/boxes/event-box/event-box-link'
 
 import DmbLogo from '@/assets/images/dmb-logo.png'
 import Image from 'next/image'
+import { Metadata } from 'next'
 
-const EventsPage: React.FC = () => {
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: `my-dMb App | Events`,
+  }
+}
+
+const EventsPage = () => {
   return (
     <DashboardWrapper>
       <div className={'mx-auto mt-2 flex flex-col items-center'}>
@@ -17,12 +24,14 @@ const EventsPage: React.FC = () => {
           alt={'dmb-logo'}
           width={150}
           height={150}
-          className={'mb-4 p-4 bg-black/35'}
+          className={'mb-4 p-4 bg-black/35 rounded shadow-md'}
         />
 
         <h1 className={'text-2xl uppercase font-bold mb-4'}>
           Wydarzenia systemowe
         </h1>
+
+        <Separator className={'my-2'} />
 
         <EventBox>
           <EventBoxHeader>DIAMOND LIVE</EventBoxHeader>
@@ -51,6 +60,30 @@ const EventsPage: React.FC = () => {
 
           <EventBoxLink url={'https://us06web.zoom.us/j/89796337110'} />
         </EventBox>
+
+        <Separator className={'my-2'} />
+
+        <EventBox>
+          <EventBoxHeader>INTERNATIONAL AWARD CEREMONY</EventBoxHeader>
+
+          <EventBoxSubheader>
+            pierwsza niedziela miesiąca, godz. 19:00
+          </EventBoxSubheader>
+
+          <EventBoxLink url={'https://us06web.zoom.us/j/88601812540'} />
+        </EventBox>
+
+        <Separator className={'my-2'} />
+
+        <EventBox>
+          <EventBoxHeader>WIECZÓR Z EKSPERTEM</EventBoxHeader>
+
+          <EventBoxSubheader>czwartek, godz. 20:00</EventBoxSubheader>
+
+          <EventBoxLink url={'https://us02web.zoom.us/j/85906635318'} />
+        </EventBox>
+
+        <Separator className={'my-2'} />
       </div>
     </DashboardWrapper>
   )
--- src/components/ui/boxes/event-box/event-box.tsx
+++ src/components/ui/boxes/event-box/event-box.tsx
@@ -2,7 +2,11 @@ import { EventBoxProps } from '@/components/ui/boxes/event-box/event-box.types'
 
 export const EventBox = ({ children }: EventBoxProps) => {
   return (
-    <div className={'flex flex-col items-center bg-white/25 p-2 min-w-full'}>
+    <div
+      className={
+        'flex flex-col items-center bg-white/25 p-2 min-w-full rounded shadow-md hover:shadow-lg transition-shadow hover:bg-white/35'
+      }
+    >
       {children}
     </div>
   )
