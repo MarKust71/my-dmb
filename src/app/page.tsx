bcd5a20c61a1ac5c388f741fdb1ad442969338c3
Marek Kustosz <markust71@hotmail.com>
2024-07-14 19:07:42 +0200

    set up new home page (#99)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,38 @@
 import { Poppins } from 'next/font/google'
 import { cn } from '@/lib/utils'
-import { Button } from '@/components/ui/button'
-import { LoginButton } from '@/components/auth/login-button'
-import { redirect } from 'next/navigation'
-import { DEFAULT_REDIRECT } from '@/routes'
+import Image from 'next/image'
+import DmbLogo from '@/assets/images/dmb-logo.png'
+import './page.scss'
 
 const font = Poppins({
   subsets: ['latin'],
-  weight: ['600'],
+  weight: ['400'],
 })
 
 export default function Home() {
-  redirect(DEFAULT_REDIRECT)
-
   return (
     <>
       <main
-        className={
-          'flex h-full flex-col items-center justify-center text-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800'
-        }
+        className={cn(
+          'flex h-full flex-col items-center justify-center text-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800',
+          font.className
+        )}
       >
-        <div className={'space-y-6 text-center'}>
-          <h1
-            className={cn(
-              'text-6xl font-semibold text-white drop-shadow-md',
-              font.className
-            )}
-          >
-            🔐 Auth
-          </h1>
+        <Image src={DmbLogo} alt={'dmb-logo'} className={'mb-16'} />
 
-          <div>
-            <LoginButton>
-              <Button variant={'secondary'} size={'lg'}>
-                Sign In
-              </Button>
-            </LoginButton>
-          </div>
-        </div>
+        <h1 className={'text-2xl mb-4 gold-text-gradient no-antialiasing'}>
+          Największa na świecie społeczność osób ambitnych
+          i&nbsp;przedsiębiorczych
+        </h1>
+
+        <h1 className={'text-2xl mb-4 gold-text-gradient no-antialiasing'}>
+          oraz
+        </h1>
+
+        <h1 className={'text-2xl gold-text-gradient no-antialiasing'}>
+          system wsparcia dla pragnących sukcesów, niezależności finansowej,
+          zdrowia i&nbsp;urody.
+        </h1>
       </main>
     </>
   )
