d7ac680e7a7becebbac40a40c4c510636bc3038e
Marek Kustosz <markust71@hotmail.com>
2024-09-29 15:05:12 +0200

    set up media page - update

--- next.config.js
+++ next.config.js
@@ -43,6 +43,12 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'images.unsplash.com',
         port: '',
         pathname: '/**',
--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -1,23 +1,25 @@
 const Component = () => {
   const FIREBASE_STORAGE_URL =
-    'https://firebasestorage.googleapis.com/v0/b/my-dmb.appspot.com/o/media%2FdMb-Business-Plan.mp4?alt=media&token=2cf899d0-43c0-4a18-bf0c-03623014d76a'
+    'https://firebasestorage.googleapis.com/v0/b/my-dmb.appspot.com/o/media%2FdMb-Business-Plan.mp4?alt=media'
+  const BACKGROUND_IMAGE_URL =
+    'https://firebasestorage.googleapis.com/v0/b/my-dmb.appspot.com/o/media%2Fimage%2Fpower-campus-2024-10-zoom-background.jpeg?alt=media'
 
   return (
-    <>
-      {/*
-      <iframe
-        src="https://drive.google.com/file/d/1jch2so5_EYMWX4ITYtT6yCNGePGH6p4V/preview"
+    <div
+      className={`min-h-screen bg-blue-200 bg-cover bg-no-repeat flex flex-col pt-48`}
+      style={{ backgroundImage: `url(${BACKGROUND_IMAGE_URL})` }}
+    >
+      <video
         width="960"
         height="720"
-        allow="autoplay"
-      ></iframe>
-*/}
-
-      <video width="960" height="720" controls controlsList="nodownload">
+        controls
+        controlsList="nodownload"
+        className={'p-4 md:p-12'}
+      >
         <source src={FIREBASE_STORAGE_URL} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-    </>
+    </div>
   )
 }
 
