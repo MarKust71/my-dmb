e13f3eda86467cf427450ba7130451a932866681
Marek Kustosz <markust71@hotmail.com>
2025-02-23 13:48:36 +0100

    set up database schema and error handling for customer retrieval

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,19 @@
 import { PrismaClient } from '@prisma/client'
+import { createClient } from '@libsql/client'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
+
+const libsql = createClient({
+  url: `${ process.env.TURSO_DATABASE_URL }`,
+  authToken: `${ process.env.TURSO_AUTH_TOKEN }`,
+})
+const adapter = new PrismaLibSQL(libsql)
 
 declare global {
   var prisma: PrismaClient | undefined
 }
 
-export const db = globalThis.prisma || new PrismaClient()
+export const db = globalThis.prisma || new PrismaClient({ adapter })
+// export const db = globalThis.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') {
   globalThis.prisma = db
