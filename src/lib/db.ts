a351e036f92dba6aea85e5d99f2c0ee59d10339b
Marek Kustosz <markust71@hotmail.com>
2025-09-14 15:33:17 +0200

    Refactor database client initialization for clarity and maintainability

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,12 +2,14 @@ import { PrismaClient } from '@prisma/client'
 import { createClient } from '@libsql/client'
 import { PrismaLibSQL } from '@prisma/adapter-libsql'
 
+/*
 const libsql = createClient({
   url: `${process.env.TURSO_DATABASE_URL}`,
   authToken: `${process.env.TURSO_AUTH_TOKEN}`,
 })
 
-// const adapter = new PrismaLibSQL(libsql)
+const adapter = new PrismaLibSQL(libsql)
+*/
 const adapter = new PrismaLibSQL({
   url: `${process.env.TURSO_DATABASE_URL}`,
   authToken: `${process.env.TURSO_AUTH_TOKEN}`,
