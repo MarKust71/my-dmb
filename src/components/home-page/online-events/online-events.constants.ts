cb310dd75f77fa9974ca528244a24ddb5528a00f
Marek Kustosz <markust71@hotmail.com>
2026-05-24 13:37:51 +0200

    Update event images to use Firebase storage links and add Firebase console link to README

--- src/components/home-page/online-events/online-events.constants.ts
+++ src/components/home-page/online-events/online-events.constants.ts
@@ -9,7 +9,8 @@ export const ONLINE_EVENTS: HomePageEvent[] = [
     timeEnd: '21:00',
     location: 'zoom.us/j/89796337110',
     href: 'https://zoom.us/j/89796337110',
-    image: '/img/event/diamond-live-event.jpeg',
+    image:
+      'https://firebasestorage.googleapis.com/v0/b/my-dmb.appspot.com/o/media%2Fimage%2Fevent%2Fdiamond-live-event-1.jpeg?alt=media&token=c8dbc85e-6b6c-44c9-bcb5-e9a1961911b1',
     recurrence: 'WEEKLY',
   },
   {
@@ -20,7 +21,8 @@ export const ONLINE_EVENTS: HomePageEvent[] = [
     timeEnd: '10:00',
     location: 'zoom.us/j/981646663',
     href: 'https://zoom.us/j/981646663',
-    image: '/img/event/biznes-espresso-event.jpeg',
+    image:
+      'https://firebasestorage.googleapis.com/v0/b/my-dmb.appspot.com/o/media%2Fimage%2Fevent%2Fbiznes-espresso-event.jpeg?alt=media&token=4c49c7a5-1ab1-4728-8508-a90be6c0f0f2',
     recurrence: 'WEEKLY',
   },
 ]
